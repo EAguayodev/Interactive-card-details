d41320f197e3232daed39332e6603f919a0b2fd8
Eric Aguayo <computergeek90@protonmail.com>
2023-08-21 18:06:21 -0500

    first commit push

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// const nameInput = document.getElementById('js__name').style.border = '1px solid #FF5050';
+// const numberInput = document.getElementById('js__number').style.border = '1px solid #FF5050';
+// const monthInput = document.getElementById('js__month').style.border = '1px solid #FF5050';
+// const yearInput = document.getElementById('js__year').style.border = '1px solid #FF5050';
+// const scInput = document.getElementById('js__sc').style.border = '1px solid #FF5050';
+
+// const errorText = document.getElementById('error-name').innerHTML = 'Wrong format, name only';
+// document.getElementById('error-number').innerHTML = 'Wrong format, numbers only';
+// const errorMonth = document.getElementById('error-month').innerHTML = "Can't be blank";
+// const errorSC = document.getElementById('error-sc').innerHTML = "Can't be blank";
+
+
